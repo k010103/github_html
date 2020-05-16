6c049e02f2f7cd1fa5d8f72c3136246c44ebf8e2
k010103 <k010103@naver.com>
2020-05-16 20:31:33 +0900

    생활코딩

---
+++ phpjs/2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <!-- Javascript에서는 문자를 더할때도 +, 숫자를 더할때도 + -->
+  <h1>Javascript</h1>
+  <script>
+    document.write(10+10);
+    document.write("<br/>10"+"10");
+  </script>
+  <!-- php상으로는 문자열도 더하기가 되므로 문자을 더할때는 . 사용-->
+  <h2>php</h2>
+  <?php
+    echo "10"+"10";
+    echo "<br>\n10"."10";
+    // <br> : 웹 페이지상으로 줄바꿈, \n : 웹페이지에서 확인하는 코드상으로 줄바꿈
+    // 계산식옆에 줄바꿈을 붙힐려고하니 오류가 발생한다. 어떻게해야 해결할 수 있을까?
+  ?>
+</body>
+</html>
---
+++ phpjs/3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <script type="text/javascript">
+    docueent.write("hello world");
+  </script>
+  <!-- 1. 크롬기준 : ctrl + shift + j , 를 이용하면 오류를 찾을 수 있음 -->
+  <!-- 2. 혹은  apache2 > long > error 파일에서 확인해볼 수 있음 -->
+
+  <!-- 3. 제일 좋은 방법은  php.ini 파일을 열어서 display_errors를 off에서 on으로 바꾸고-->
+  <!-- manager- 파일 연 다음 apache web Server Restart하기 -->
+</body>
+</html>
