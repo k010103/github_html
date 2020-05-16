8556fd6583865ddf1dff218f9f35cde7f3349805
k010103 <k010103@naver.com>
2020-05-16 20:59:58 +0900

    생활코딩 : 39. JS, PHP  6 : 변수

---
+++ phpjs/4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1>Javascript</h1>
+  <script type="text/javascript">
+    name = "junmo";
+    document.write("안녕하세요. "+name+"입니다.");
+  </script>
+  <h2>php</h2>
+  <?php
+    $name = "junmo";
+    echo "안녕하세요. ".$name."입니다.";
+   ?>
+</body>
+</html>
