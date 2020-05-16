5b0848cc53d79646f67070daa7ef333af00811cf
junmo <50203674+k010103@users.noreply.github.com>
2020-05-17 02:51:07 +0900

    생활코딩

--- phpjs/4.php
+++ phpjs/4.php
@@ -7,12 +7,12 @@
   <h1>Javascript</h1>
   <script type="text/javascript">
     name = "junmo";
-    document.write("안녕하세요. "+name+"입니다.");
+    document.write("안녕하세요 "+name+"입니다.");
   </script>
   <h2>php</h2>
   <?php
     $name = "junmo";
-    echo "안녕하세요. ".$name."입니다.";
+    echo "안녕하세요 ".$name."입니다.";
    ?>
 </body>
 </html>
